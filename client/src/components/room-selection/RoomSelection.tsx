4549cde583206d5c01da9bab1b43ef8a25cf261f
Michael Wilson <nosliw.michael@gmail.com>
2025-01-16 16:15:12 -0600

    Remove unused import

--- client/src/components/room-selection/RoomSelection.tsx
+++ client/src/components/room-selection/RoomSelection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./RoomSelection.css";
 import Quizzer from "../quizzer/Quizzer";
 import { useSocket } from "../providers/SocketProvider";
